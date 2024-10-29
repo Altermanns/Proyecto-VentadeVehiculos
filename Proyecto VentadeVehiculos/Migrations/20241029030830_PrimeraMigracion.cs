34e269f3ea0cf8f7952242d7f8ddf20d48b8af14
Altermanns <isaac.277.trujillo@gmail.com>
2024-10-28 22:12:39 -0500

    Base de datos funcional

---
+++ Proyecto VentadeVehiculos/Migrations/20241029030830_PrimeraMigracion.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Proyecto_VentadeVehiculos.Migrations
+{
+    /// <inheritdoc />
+    public partial class PrimeraMigracion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Comprador",
+                columns: table => new
+                {
+                    IdComprador = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    correo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    fechanacimiento = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comprador", x => x.IdComprador);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vehiculo",
+                columns: table => new
+                {
+                    IdVehiculo = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Marca = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Modelo = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Año = table.Column<int>(type: "int", nullable: false),
+                    precio = table.Column<double>(type: "float", nullable: false),
+                    kilometraje = table.Column<float>(type: "real", nullable: false),
+                    TipoCombustible = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Ubicacion = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehiculo", x => x.IdVehiculo);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vendedor",
+                columns: table => new
+                {
+                    IdVendedor = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    correo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    fechanacimiento = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vendedor", x => x.IdVendedor);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transaccion",
+                columns: table => new
+                {
+                    IdTransaccion = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdVendedor = table.Column<int>(type: "int", nullable: false),
+                    IdComprador = table.Column<int>(type: "int", nullable: false),
+                    IdVehiculo = table.Column<int>(type: "int", nullable: false),
+                    FechaTransaccion = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PrecioFinal = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transaccion", x => x.IdTransaccion);
+                    table.ForeignKey(
+                        name: "FK_Transaccion_Comprador_IdComprador",
+                        column: x => x.IdComprador,
+                        principalTable: "Comprador",
+                        principalColumn: "IdComprador",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Transaccion_Vehiculo_IdVehiculo",
+                        column: x => x.IdVehiculo,
+                        principalTable: "Vehiculo",
+                        principalColumn: "IdVehiculo",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Transaccion_Vendedor_IdVendedor",
+                        column: x => x.IdVendedor,
+                        principalTable: "Vendedor",
+                        principalColumn: "IdVendedor",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transaccion_IdComprador",
+                table: "Transaccion",
+                column: "IdComprador");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transaccion_IdVehiculo",
+                table: "Transaccion",
+                column: "IdVehiculo");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transaccion_IdVendedor",
+                table: "Transaccion",
+                column: "IdVendedor");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transaccion");
+
+            migrationBuilder.DropTable(
+                name: "Comprador");
+
+            migrationBuilder.DropTable(
+                name: "Vehiculo");
+
+            migrationBuilder.DropTable(
+                name: "Vendedor");
+        }
+    }
+}
